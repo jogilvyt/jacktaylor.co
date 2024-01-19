0ba6446e584e6b40591d57a270066a94d8839db7
Jack Taylor <jack.s.o.taylor@googlemail.com>
2024-01-19 22:08:19 +0000

    Add await

--- tests/e2e/smoke.test.ts
+++ tests/e2e/smoke.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test'
 
 test('the page loads', async ({ page }) => {
 	await page.goto('/')
-	expect(page.getByText('Home page')).toBeVisible()
+	await expect(page.getByText('Home page')).toBeVisible()
 })
