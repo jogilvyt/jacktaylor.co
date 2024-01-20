87c252e93795670611b2e944a5ec474eecbd95e2
Jack Taylor <jack.s.o.taylor@googlemail.com>
2024-01-20 16:10:18 +0000

    Trying something else

--- remix.config.js
+++ remix.config.js
@@ -11,7 +11,7 @@ export default {
 	tailwind: true,
 	postcss: true,
 	watchPaths: ['./tailwind.config.ts'],
-	serverDependenciesToBundle: [/^mdx-bundler.*/],
+	serverDependenciesToBundle: [/^mdx-bundler\/dist.*/],
 	routes: async defineRoutes => {
 		return flatRoutes('routes', defineRoutes, {
 			ignoredRouteFiles: [
