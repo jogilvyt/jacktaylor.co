770f18c0adb12740c4e5e4e6b013ce8fd09182cc
Jack Taylor <jack.s.o.taylor@googlemail.com>
2024-01-21 15:47:32 +0000

    Add db check back to healthcheck

--- app/routes/resources+/healthcheck.tsx
+++ app/routes/resources+/healthcheck.tsx
@@ -1,5 +1,6 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import { type DataFunctionArgs } from '@remix-run/node'
+import { prisma } from '#app/utils/db.server'
 
 export async function loader({ request }: DataFunctionArgs) {
 	const host =
@@ -9,8 +10,7 @@ export async function loader({ request }: DataFunctionArgs) {
 		// if we can connect to the database and make a simple query
 		// and make a HEAD request to ourselves, then we're good.
 		await Promise.all([
-			// TODO: make this request once we have a table to query
-			// prisma.user.count(),
+			prisma.post.count(),
 			fetch(`${new URL(request.url).protocol}${host}`, {
 				method: 'HEAD',
 				headers: { 'X-Healthcheck': 'true' },
